c7bbc6eb1f131dd2f48c7c7a33cb9dd0553ac905
Smruti Tatavarthy <statavarthy@luc.edu>
2015-07-20 20:53:27 -0500

    Created a new project as a Welcome screen to the GUI

---
+++ Charts-LifeExpectancy/Charts-LifeExpectancy/InvokeCharts.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public class InvokeCharts
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public void startProject()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new LifeExpectancy_Vs_Community());
+        }
+    }
+}
